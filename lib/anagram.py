d5fc04225fa4c7ca0a294f1f0d67b102ee8fe90e
Glorfindel-Noldor <131824775+Glorfindel-Noldor@users.noreply.github.com>
2024-05-25 10:33:53 -0500

    sorting

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,7 @@
 # your code goes here!
+class Anagram():
+    def __init__(self, arg_word):
+        self.word = sorted(arg_word.lower())
+
+    def match(self, sample):
+        return [w for w in sample if sorted(w.lower()) == self.word]
